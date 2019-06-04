09584b3d1dd2e50868498b2b6e84a96ccaac12c2
icepants <shawnluther99@gmail.com>
2019-06-03 22:57:34 -0400

    Seperated the button functionality by their topics, now when you click a button, you will get the topic that button represents as a gif!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-    // The URL we want to request from GIPHY.
-    var queryURL = "https://api.giphy.com/v1/gifs/random?&api_key=icyGNe9304U63z9nyeNrf6m75D7Wf2yP&tag= " + topics + "";
-
     // My innermost thoughts.
     var topics = [
         "computers",
@@ -11,20 +8,17 @@ $(document).ready(function () {
         "youtube",
         "food",
         "applebees",
-        "drinks",
-        "whiskysour",
         "music",
     ];
 
-    // This loop appends a button for each string in the 'topics' array.
+    // This loop prepends a button for each string in the 'topics' array.
     for (var i = 0; i < topics.length; i++) {
         // Create a button.
-        var btn = $("<input>");
+        var btn = $("<button>" + topics[i] + "</button>");
 
         // btn attributes.
-        btn.attr("type", "button");
         btn.attr("class", "btn btn-primary m-2 btnTopic");
-        btn.attr("value", topics[i]);
+        btn.attr("data-topic", topics[i]);
 
         // Populate Buttons.
         $("#btnDisplay").prepend(btn);
@@ -32,27 +26,31 @@ $(document).ready(function () {
 
     // When the user clicks on a button, populate the topic to 'thoughtPopulation'.
     $(".btnTopic").on("click", function () {
+        // The URL we want to request from GIPHY.
+        var dataTopic = $(this).attr("data-topic");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dataTopic + "&api_key=icyGNe9304U63z9nyeNrf6m75D7Wf2yP";
+        
         // Request API Data.
         $.ajax({
             url: queryURL,
-            type: "GET",
-            success: function (response) {
-                console.log(response);
+            type: "GET"
+        }).then(function (response) {
+            console.log(response.data);
+            var results = response.data[0].images;
 
-                // Store gifs in this variable.
-                var imageURL = response.data.fixed_width_small_still_url;
+            // Store gifs in this variable.
+            var imageURL = results.downsized_still.url;
 
-                // Create image element.
-                var img = $("<img height='150' width='150'>");
+            // Create image element.
+            var img = $("<img height='150' width='150'>");
 
-                // Image Attributes.
-                img.attr("src", imageURL);
-                img.attr("alt", "Image");
-                img.attr("style", "padding: 5px;")
+            // Image Attributes.
+            img.attr("src", imageURL);
+            img.attr("alt", "Image");
+            img.attr("style", "padding: 5px;")
 
-                // Populate image.
-                $("#thoughtPopulation").prepend(img);
-            }
+            // Populate image.
+            $("#thoughtPopulation").prepend(img);
         })
     })
 });
